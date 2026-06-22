349b155fe377317c576905c2d19d88117fa2886f
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-06-22 20:19:44 +0300

    реализована возможность добавлять новую задачу в класс TaskTracker и выводить список всех задач

--- src/main/java/TaskTracker.java
+++ src/main/java/TaskTracker.java
@@ -1,2 +1,20 @@
+import java.util.List;
+
 public class TaskTracker {
+    private List<Task> tasks;
+
+    public TaskTracker(List<Task> tasks) {
+        this.tasks = tasks;
+            }
+
+            public void addTask(String title) {
+        tasks.add(new Task(title));
+                System.out.println("Задача " + title + "добавлена");
+            }
+
+            public void printAllTask() {
+        for (Task task : tasks) {
+            task.printInfo();
+        }
+            }
 }
