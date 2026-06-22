6d0759622af9b484da0bd7a3230739d85dc3a724
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-06-22 19:25:31 +0300

    созданы конструкторы класса (Мой первый коммит)

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Task {
+private String title;
+private boolean isCompleted;
+
+//Создаем конструктор класса
+public Task(String title) {
+    this.title = title;
+    this.isCompleted = false;
 }
+
+public void complete() {
+    this.isCompleted = true;
+}
+}
+
