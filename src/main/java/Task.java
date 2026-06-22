1a5aef28f7ea6e1f5c3ae8a9abeea7e163385b76
Alevtina Petrova <alevtina.piatrova@gmail.com>
2026-06-22 19:56:25 +0300

    описаны методы класса Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,8 +8,25 @@ public Task(String title) {
     this.isCompleted = false;
 }
 
+//метод помечает задачу как выполнена
 public void complete() {
     this.isCompleted = true;
 }
+
+//метод получает статус задачи
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+//метод получает название задачи
+    public String getTitle() {
+        return title;
+    }
+
+    //Выводим информацию о задаче
+    public void printInfo() {
+        String status = isCompleted ? "[ ]" : "[x]";
+        System.out.println(status + " " + title);
+    }
 }
 
